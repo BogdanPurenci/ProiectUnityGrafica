0722806a470bc3264f0f0464d2937ea8df2f6f73
BogdanPurenci <bogdanpurenci@gmail.com>
2024-01-08 16:15:55 +0200

    Proiect

--- Assets/Scripts/TranslateSphere.cs
+++ Assets/Scripts/TranslateSphere.cs
@@ -7,16 +7,19 @@ public class TranslateSphere : MonoBehaviour
     public float speed = 2f;
     private float timer = 0f;
     private bool moveRight = true;
+    private float deformOffset = 0.005f;
 
     private void Update()
     {
         if (moveRight)
         {
             transform.Translate(Vector3.right * speed * Time.deltaTime);
+            transform.localScale += new Vector3(deformOffset, 0, 0);
         }
         else
         {
             transform.Translate(Vector3.left * speed * Time.deltaTime);
+            transform.localScale -= new Vector3(deformOffset, 0, 0);
         }
 
         timer += Time.deltaTime;
